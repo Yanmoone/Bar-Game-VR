4b0b012d6cb1d85cada26a8a847dab59bd8672f0
Yanmoone <yanuhue@hotmail.com>
2019-04-16 14:20:20 +0200

    Update

---
+++ Bar Game VR/Assets/Scripts/Pooring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooring : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _water;
+
+    [SerializeField]
+    private Transform _pooringPosition;
+
+    void Update()
+    {
+        if (transform.eulerAngles.x > 80 && transform.eulerAngles.x < 280 || transform.eulerAngles.z > 80 && transform.eulerAngles.z < 280)
+        {
+            Debug.Log("klunk");
+            Instantiate(_water, _pooringPosition.position, transform.rotation);
+
+        }
+    }
+}
