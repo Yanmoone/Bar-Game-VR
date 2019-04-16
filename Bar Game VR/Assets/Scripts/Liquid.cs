8ccca6d1a8175e322872c94a10232ca2834e85e2
Flashcaptain <36227316+Flashcaptain@users.noreply.github.com>
2019-04-16 15:21:13 +0200

    Mixing in progress

---
+++ Bar Game VR/Assets/Scripts/Liquid.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Liquid : MonoBehaviour
+{
+    public Color _color;
+    public Shader _test;
+
+    void Start()
+    {
+        _test = this.gameObject.GetComponent<Shader>();
+    }
+}
---
+++ Bar Game VR/Assets/Scripts/Mixing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mixing : MonoBehaviour
+{
+    [SerializeField]
+    private Material _material;
+
+    [SerializeField]
+    [Range(0,1)]
+    private float _maxSize = 0.9f;
+
+    [SerializeField]
+    private float _capacity = 20;
+
+    [SerializeField]
+    private GameObject _water;
+
+    private Color _color;
+    private int _liquidCount;
+
+    void OnTriggerEnter(Collider CEnter)
+    {
+        Liquid liquid = CEnter.GetComponent<Liquid>();
+
+        if (liquid == null)
+        {
+            return;
+        }
+
+        if (_capacity == _liquidCount)
+        {
+            return;
+        }
+        Debug.Log("slurp");
+        _liquidCount++;
+        _color = ((_color * _liquidCount) + liquid._color) / (_liquidCount + 1);
+        _material.color = _color;
+        _water.transform.localScale += new Vector3(0, _maxSize/_capacity, 0);
+        Debug.Log(_water.transform.position);
+        _water.transform.position += new Vector3(0, (_maxSize / _capacity)/2, 0);
+    }
+}
